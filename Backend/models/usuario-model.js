d40a4c19bccfd8629cc02c8b8273a4cd94e6781a
Josstym <ca741084@gmail.com>
2025-04-13 18:26:21 -0600

    Usuario-model funcion para registrar usuarios

--- Backend/models/usuario-model.js
+++ Backend/models/usuario-model.js
@@ -16,4 +16,47 @@ UsuarioModel.login = (nombre_usuario, password, cb) => {
   });
 };
 
+UsuarioModel.register = (data, cb) => {
+  const {
+    nombre_usuario,
+    apellido,
+    password,
+    email,
+    rol
+  } = data;
+
+  const sql = `
+    INSERT INTO EntidadUsuario (
+      nombre_usuario,
+      apellido,
+      password,
+      email,
+      estado,
+      ultima_fecha_hora_ingreso,
+      password_expira,
+      dias_caducidad_password,
+      rol,
+      numero_intentos_incorrectos,
+      fecha_registro
+    ) VALUES (
+      $1, $2, $3, $4, true, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, 90, $5, 0, CURRENT_TIMESTAMP
+    )
+    RETURNING *;
+  `;
+
+  const values = [
+    nombre_usuario,  // $1
+    apellido,        // $2
+    password,        // $3
+    email,           // $4
+    rol              // $5
+  ];
+
+  conn.query(sql, values, (err, result) => {
+    if (err) return cb(err, null);
+    cb(null, { success: true, message: "Usuario registrado correctamente", user: result.rows[0] });
+  });
+};
+
+
 module.exports = UsuarioModel;
